720aa81465b994556780f2b7dc5b1bad87f97a48
Miguel John Doherty <migueljd@outlook.com>
2015-01-24 10:48:13 -0300

    Endgame Object Script

---
+++ Assets/Scripts/MovableObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovableObject : MonoBehaviour {
+
+	public float objectSpeed = 0.5f;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate(new Vector3(0, 0,this.objectSpeed)*Time.deltaTime);
+	}
+}
