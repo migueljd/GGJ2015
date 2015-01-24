9e639545dcbc19652975d6de3bddeba1f58fafc6
Miguel John Doherty <migueljd@outlook.com>
2015-01-24 11:05:08 -0300

    Commit de endgame para o jogador

---
+++ Assets/Scripts/EndgameBody.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndgameBody : MonoBehaviour {
+
+	public int objectsNeeded = 4;
+	public string winObjectName = "Crux";
+	private int objectsCollected = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.name == winObjectName){
+			objectsCollected+=1;
+			if(objectsCollected == objectsNeeded){
+				GameControler.GameOver(this.name);
+			}
+		}
+
+	}
+}
---
+++ Assets/Scripts/GameControler.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControler : MonoBehaviour {
+
+	private static GameControler instance;
+
+	public string bodyName = "Body";
+	public string ghostName = "Ghost";
+
+	// Use this for initialization
+	void Start () {
+		instance = new GameControler();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public static void GameOver(string winnerName){
+		instance._GameOver(winnerName);
+	}
+
+	private void _GameOver(string winnerName){
+
+		Debug.Log(winnerName + " wins"));
+		//Execute Body victory
+		if(winnerName == "Body"){
+		
+		}
+		//Execute Ghost victory
+		else if(winnerName == "Ghost"){
+			
+		}
+	}
+}
