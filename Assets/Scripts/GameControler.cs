cf3266dffad8904832f444890ded1a5fb38b9cb4
Miguel John Doherty <migueljd@outlook.com>
2015-01-24 12:26:08 -0300

    Game Over screen

--- Assets/Scripts/GameControler.cs
+++ Assets/Scripts/GameControler.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GameControler : MonoBehaviour {
@@ -8,9 +9,21 @@ public class GameControler : MonoBehaviour {
 	public string bodyName = "Body";
 	public string ghostName = "Ghost";
 
+	private Text winText;
+	public Canvas canvas;
+
 	// Use this for initialization
 	void Start () {
 		instance = new GameControler();
+		instance.canvas = this.canvas;
+		instance.winText = this.winText;
+		Text[] texts = instance.canvas.GetComponentsInChildren<Text>();
+
+		instance.canvas.enabled = false;
+		foreach (Text text in texts){
+			Debug.Log(text);
+			if(text.name == "Win Text") instance.winText = text;
+		}
 	}
 	
 	// Update is called once per frame
@@ -24,14 +37,18 @@ public class GameControler : MonoBehaviour {
 
 	private void _GameOver(string winnerName){
 
+		instance.canvas.enabled = true;
+		Time.timeScale = 2;
 		Debug.Log(winnerName + " wins");
 		//Execute Body victory
 		if(winnerName == "Body"){
-		
+			instance.winText.text = "Body wins!!!";
 		}
 		//Execute Ghost victory
 		else if(winnerName == "Ghost"){
-			
+			instance.winText.text = "Ghost wins!!!";
 		}
+
+
 	}
 }
