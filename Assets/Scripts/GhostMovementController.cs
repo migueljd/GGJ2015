09ef39868ce44bc6ac05d9d32a1ee8051778701d
Jonathan Silva <jhas@gonzaguinha.local>
2015-01-24 15:47:02 -0300

    ghost movement

---
+++ Assets/Scripts/GhostMovementController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostMovementController : MonoBehaviour {
+	public float moveSpeed = 2f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		doMovement ();
+	}
+	
+	private void doMovement(){
+		float x = 0.0f;
+		float y = 0.0f;
+		float z = 0.0f;
+		if(Input.GetKey(KeyCode.LeftArrow))
+			x = -this.moveSpeed;
+		if (Input.GetKey (KeyCode.RightArrow))
+			x = this.moveSpeed;
+		if(Input.GetKey (KeyCode.UpArrow))
+			z = this.moveSpeed;
+		if (Input.GetKey (KeyCode.DownArrow))
+			z = -this.moveSpeed;
+		this.transform.Translate(new Vector3(x,y,z) *Time.deltaTime);
+	}
+}
