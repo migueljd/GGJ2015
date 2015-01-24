d5eb3be50627230e506f77b6c97d698b52cf3618
Miguel John Doherty <migueljd@outlook.com>
2015-01-24 15:49:55 -0300

    Estagio inicial(mal feito) e spawn

--- Assets/Scripts/EndgameBody.cs
+++ Assets/Scripts/EndgameBody.cs
@@ -4,12 +4,20 @@ using System.Collections;
 public class EndgameBody : MonoBehaviour {
 
 	public int objectsNeeded = 4;
-	public string winObjectName = "Crux";
+	public string winObjectTag = "WinObject";
+	public Transform winObjectPrefab;
+	public string winObjectSpawnerTag;
+
 	private int objectsCollected = 0;
+	
+	private GameObject[] spawnersList;
+	private GameObject currentSpawner;
 
 	// Use this for initialization
 	void Start () {
-	
+		spawnersList = GameObject.FindGameObjectsWithTag(this.winObjectSpawnerTag);
+		SpawnNewWinObject();
+		Debug.Log (spawnersList.Length);
 	}
 	
 	// Update is called once per frame
@@ -18,12 +26,24 @@ public class EndgameBody : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider other){
-		if(other.name == winObjectName){
+		if(other.tag == winObjectTag){
 			objectsCollected+=1;
 			if(objectsCollected == objectsNeeded){
 				GameControler.GameOver(this.name);
 			}
+			else{
+				SpawnNewWinObject();
+			}
 		}
 
+	}
+
+	void SpawnNewWinObject(){
+		GameObject tempSpawner = currentSpawner;
+		do{
+			currentSpawner = spawnersList[Random.Range(0, spawnersList.Length)];
+		}
+		while(tempSpawner != null && tempSpawner.Equals(currentSpawner));
+		Instantiate(winObjectPrefab, currentSpawner.transform.position, Quaternion.identity);	
 	}
 }
--- Assets/Scripts/EndgameObject.cs
+++ Assets/Scripts/EndgameObject.cs
@@ -5,15 +5,7 @@ public class EndgameObject : MonoBehaviour {
 
 
 
-	// Use this for initialization
-	void Start () {
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
 	void OnTriggerEnter(Collider other){
 		if(other.name == "Body"){
